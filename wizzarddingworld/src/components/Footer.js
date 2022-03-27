03fc9823a1ee23347bb5ece78ba9b0210fc4179d
Nicolás <nicolascaytuirosilva@gmail.com>
2022-03-26 21:11:30 -0500

    Se completo el Blog

---
+++ wizzarddingworld/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="main-footer">
+            <h1>Aquí va el footer</h1>
+        </div>
+    );
+}
+
+export default Footer;
